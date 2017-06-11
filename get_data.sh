d251027dca2531c5ba117f8b42c89f3812f6142d
nadbor <nadbordrozd@gmail.com>
2017-06-11 22:13:24 +0100

    data getting script

---
+++ get_data.sh
@@ -0,0 +1,5 @@
+cd data
+
+wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-all-terms.txt
+wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-all-terms.txt
+cat r8-*-all-terms.txt > r8-all-terms.txt
