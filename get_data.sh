4fefcf6220d20c2e021731a104d95f03cbeffa6b
nadbor <nadbordrozd@gmail.com>
2017-06-13 22:45:41 +0100

    download more datasets

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,30 @@
 cd data
 
-wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-all-terms.txt
-wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-all-terms.txt
-cat r8-*-all-terms.txt > r8-all-terms.txt
+if [ ! -f r8-all-terms.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-all-terms.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-all-terms.txt
+    cat r8-*-all-terms.txt > r8-all-terms.txt
+    rm r8-*-all-terms.txt
+fi
+
+if [ ! -f r52-all-terms.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-train-all-terms.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-test-all-terms.txt
+    cat r52-*-all-terms.txt > r52-all-terms.txt
+    rm r52-*-all-terms.txt
+fi
+
+if [ ! -f 20ng-all-terms.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-train-all-terms.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-test-all-terms.txt
+    cat 20ng-*-all-terms.txt > 20ng-all-terms.txt
+    rm 20ng-*-all-terms.txt
+fi
+
+
+if [ ! -f webkb-stemmed.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/webkb-train-stemmed.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/webkb-test-stemmed.txt
+    cat webkb-*-stemmed.txt > webkb-stemmed.txt
+    rm webkb-*-stemmed.txt
+fi
