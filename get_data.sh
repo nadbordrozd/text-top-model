ae68ffee3d401221c51bd0d61c915be2a1028dcb
nadbor <nadbordrozd@gmail.com>
2017-06-14 14:18:54 +0100

    more variations of reuters datasets -stemmed etc

--- get_data.sh
+++ get_data.sh
@@ -7,11 +7,53 @@ if [ ! -f r8-all-terms.txt ]; then
     rm r8-*-all-terms.txt
 fi
 
+if [ ! -f r8-no-short.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-no-short.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-no-short.txt
+    cat r8-*-no-short.txt > r8-no-short.txt
+    rm r8-*-no-short.txt
+fi
+
+if [ ! -f r8-no-stop.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-no-stop.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-no-stop.txt
+    cat r8-*-no-stop.txt > r8-no-stop.txt
+    rm r8-*-no-stop.txt
+fi
+
+if [ ! -f r8-stemmed.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-train-stemmed.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets//r8-test-stemmed.txt
+    cat r8-*-stemmed.txt > r8-stemmed.txt
+    rm r8-*-stemmed.txt
+fi
+
 if [ ! -f r52-all-terms.txt ]; then
     wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-train-all-terms.txt
     wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-test-all-terms.txt
     cat r52-*-all-terms.txt > r52-all-terms.txt
     rm r52-*-all-terms.txt
+fi
+
+if [ ! -f r52-no-short.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-train-no-short.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-test-no-short.txt
+    cat r52-*-no-short.txt > r52-no-short.txt
+    rm r52-*-no-short.txt
+fi
+
+if [ ! -f r52-no-stop.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-train-no-stop.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-test-no-stop.txt
+    cat r52-*-no-stop.txt > r52-no-stop.txt
+    rm r52-*-no-stop.txt
+fi
+
+if [ ! -f r52-stemmed.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-train-stemmed.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/r52-test-stemmed.txt
+    cat r52-*-stemmed.txt > r52-stemmed.txt
+    rm r52-*-stemmed.txt
 fi
 
 if [ ! -f 20ng-all-terms.txt ]; then
@@ -21,6 +63,27 @@ if [ ! -f 20ng-all-terms.txt ]; then
     rm 20ng-*-all-terms.txt
 fi
 
+if [ ! -f 20ng-no-short.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-train-no-short.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-test-no-short.txt
+    cat 20ng-*-no-short.txt > 20ng-no-short.txt
+    rm 20ng-*-no-short.txt
+fi
+
+if [ ! -f 20ng-no-stop.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-train-no-stop.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-test-no-stop.txt
+    cat 20ng-*-no-stop.txt > 20ng-no-stop.txt
+    rm 20ng-*-no-stop.txt
+fi
+
+if [ ! -f 20ng-stemmed.txt ]; then
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-train-stemmed.txt
+    wget http://www.cs.umb.edu/~smimarog/textmining/datasets/20ng-test-stemmed.txt
+    cat 20ng-*-stemmed.txt > 20ng-stemmed.txt
+    rm 20ng-*-stemmed.txt
+fi
+
 
 if [ ! -f webkb-stemmed.txt ]; then
     wget http://www.cs.umb.edu/~smimarog/textmining/datasets/webkb-train-stemmed.txt
@@ -28,3 +91,5 @@ if [ ! -f webkb-stemmed.txt ]; then
     cat webkb-*-stemmed.txt > webkb-stemmed.txt
     rm webkb-*-stemmed.txt
 fi
+
+
