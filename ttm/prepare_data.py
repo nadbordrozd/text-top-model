8ba4e54c23ead93c8c7ad9d018e3eaca5a8dbcd9
nadbor <nadbordrozd@gmail.com>
2017-06-24 22:01:32 +0100

    added function to prepare word embeddings

--- ttm/prepare_data.py
+++ ttm/prepare_data.py
@@ -34,3 +34,21 @@ def prepare_dataset(path):
     word2ind = {w: i for i, w in enumerate(vocab)}
     X = np.array([[word2ind[w] for w in tokens] for tokens in X])
     return X, labels, vocab, label_encoder
+
+
+@cache
+def get_embedding_matrix(vocab, embedding_path):
+    word2ind = {w: i for i, w in enumerate(vocab)}
+    with open(embedding_path, 'rb') as f:
+        embedding_dim = len(f.readline().split()) - 1
+    embeddings = np.random.normal(size=(len(vocab), embedding_dim))
+
+    with open(embedding_path, 'rb') as f:
+        for line in f:
+            parts = line.split()
+            word = parts[0]
+            if word in word2ind:
+                i = word2ind[word]
+                vec = np.array([float(x) for x in parts[1:]])
+                embeddings[i] = vec
+    return embeddings
