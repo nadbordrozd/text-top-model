23ea022ac4cf1682bf0b84923e545ee2c530e77c
nadbor <nadbordrozd@gmail.com>
2017-08-12 18:58:34 +0100

    fixed venv

--- ttm/instance_setup.sh
+++ ttm/instance_setup.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env bash
+# ami-f0bde196
+sudo apt-get install unzip
+sudo apt-get -y install virtualenv
 
 export LC_ALL=C
 pip install --upgrade pip
-
-pip install joblib
-pip install unidecode
-pip install hyperopt
-
-pip uninstall keras
-sudo pip uninstall keras
-pip install --upgrade keras
 
 mkdir workspace
 cd workspace
 git clone https://github.com/nadbordrozd/text-top-model.git
 cd text-top-model
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 bash get_data.sh
 python prepare_polarity_dataset.py
 python prepare_subjectivity_dataset.py
-
-sudo apt-get install unzip
-
-
-sudo apt-get install zsh
-sudo chsh -s /bin/zsh ubuntu
-echo "export PATH=/usr/local/cuda/bin:$PATH" > ~/.zshrc
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
